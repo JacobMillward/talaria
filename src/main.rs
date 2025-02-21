8e9f146091f7026b35addf4bde924cef3d057fb5
Jacob Millward-Yuan <jacob@millward.dev>
2025-02-21 23:09:33 +0000

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use dioxus::prelude::*;
+
+use components::Hero;
+
+mod components;
+
+const FAVICON: Asset = asset!("/assets/favicon.ico");
+const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
+
+fn main() {
+    dioxus::launch(App);
+}
+
+#[component]
+fn App() -> Element {
+    // Build cool things ✌️
+
+    rsx! {
+        // Global app resources
+        document::Link { rel: "icon", href: FAVICON }
+        document::Link { rel: "stylesheet", href: TAILWIND_CSS }
+
+        Hero {}
+
+    }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+export const mode = "all";
+export const content = ["./src/**/*.{rs,html,css}", "./dist/**/*.html"];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
